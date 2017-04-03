019146346300ed27c133a797e43c0e60cbb34ff7
Trygve Laugstøl <trygvis@inamo.no>
2017-04-03 21:55:58 +0200

    o Renaming the 'api' blueprint to 'core' so we can have a stable API later on.

--- web/p2k16_web/__init__.py
+++ web/p2k16_web/__init__.py
@@ -1,10 +1,40 @@
+import flask
 import flask_bower
-
+import os
 import p2k16.database
-import p2k16_web.views
+import p2k16_web.core_blueprint
 
 app = p2k16.app
 
-app.register_blueprint(p2k16_web.views.api)
+
+@app.url_defaults
+def hashed_url_for_static_file(endpoint, values):
+    if 'static' == endpoint or endpoint.endswith('.static'):
+        filename = values.get('filename')
+        if filename:
+            if '.' in endpoint:  # has higher priority
+                blueprint = endpoint.rsplit('.', 1)[0]
+            else:
+                blueprint = flask.request.blueprint  # can be None too
+
+            static_folder=None
+            if blueprint:
+                static_folder = app.blueprints[blueprint].static_folder
+
+            if not static_folder:
+                static_folder = app.static_folder
+
+            param_name = 'h'
+            while param_name in values:
+                param_name = '_' + param_name
+
+            values[param_name] = static_file_hash(os.path.join(static_folder, filename))
+
+
+def static_file_hash(filename):
+    return int(os.stat(filename).st_mtime)  # or app.config['last_build_timestamp'] or md5(filename) or etc...
+
+
+app.register_blueprint(p2k16_web.core_blueprint.core)
 
 flask_bower.Bower(app)
--- web/p2k16_web/core_blueprint.py
+++ web/p2k16_web/core_blueprint.py
@@ -7,7 +7,7 @@ from p2k16.database import db
 from p2k16.models import User
 from p2k16 import app
 
-api = Blueprint('api', __name__, template_folder='templates')
+core = Blueprint('core', __name__, template_folder='templates')
 
 
 def user_to_json(user):
@@ -18,13 +18,13 @@ def user_to_json(user):
     }
 
 
-@api.route('/data/user')
+@core.route('/data/user')
 def data_user():
     users = [user_to_json(user) for user in User.query.all()]
     return jsonify(users)
 
 
-@api.route('/data/user/<int:user_id>', methods=['POST'])
+@core.route('/data/user/<int:user_id>', methods=['POST'])
 def data_user_port(user_id):
     user = User.find_user_by_id(user_id)
 
@@ -36,20 +36,20 @@ def data_user_port(user_id):
     return user_to_json(user)
 
 
-@api.route('/')
+@core.route('/')
 def index():
     user = flask_login.current_user.is_authenticated and user_to_json(flask_login.current_user.user)
 
     return render_template('index.html', user=user)
 
 
-@api.route('/logout', methods=['GET'])
+@core.route('/logout', methods=['GET'])
 def logout():
     flask_login.logout_user()
-    return flask.redirect(flask.url_for('api.index'))
+    return flask.redirect(flask.url_for('core.index'))
 
 
-@api.route('/login', methods=['GET', 'POST'])
+@core.route('/login', methods=['GET', 'POST'])
 def login():
     if flask.request.method == 'GET':
         show_message = flask.request.args.get('show_message') or ''
@@ -63,12 +63,12 @@ def login():
         app.logger.info("user {} logged in".format(username))
         authenticated_user = auth.AuthenticatedUser(user)
         flask_login.login_user(authenticated_user)
-        return flask.redirect(flask.url_for('api.index'))
+        return flask.redirect(flask.url_for('core.index'))
 
     return flask.redirect(flask.url_for('.login', show_message='bad-login', username=username))
 
 
-@api.route('/reset-password', methods=['POST'])
+@core.route('/reset-password', methods=['POST'])
 def reset_password():
     username = flask.request.form.get('username')
     if username is not None:
@@ -80,7 +80,7 @@ def reset_password():
     return flask.redirect(flask.url_for('.login', show_message='recovery', username=username))
 
 
-@api.route('/protected')
+@core.route('/protected')
 @flask_login.login_required
 def protected():
     u = flask_login.current_user.user

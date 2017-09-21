3f2044d00f8db1265be973f3871e05204b6ae00a
Trygve Laugstøl <trygvis@inamo.no>
2017-09-21 13:49:20 +0200

    o Moving code under src/, tests under test/. o Making setup.py so we can use pip. o Moving app dependencies into setup.py.

---
+++ web/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(name='p2k16',
+      version='1.0',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      entry_points={
+      },
+      install_requires=[
+          'flask',
+          'flask-login',
+          'flask-bcrypt',
+          'Flask-Bower',
+          'Flask-SQLAlchemy',
+          'Flask-Testing',
+          'flask-mail',
+          'psycopg2',
+          'sqlalchemy',
+          'sqlalchemy-migrate',
+          'nose',
+          'paho-mqtt',
+      ],
+      )

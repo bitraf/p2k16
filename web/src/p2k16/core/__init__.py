5e4fa12eb6ab25689565d3f843a45437fed47039
Trygve Laugstøl <trygvis@inamo.no>
2018-01-02 23:20:44 +0100

    o Support reading configuration values from the environment.

--- web/src/p2k16/core/__init__.py
+++ web/src/p2k16/core/__init__.py
@@ -1,18 +1,11 @@
 import os
 
 from flask import Flask
+from flask_env import MetaFlaskEnv
 
-app = Flask(__name__, static_folder="../web/static")
 
-app.config.BOWER_KEEP_DEPRECATED = False
-app.config['BOWER_COMPONENTS_ROOT'] = '../web/bower_components'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-p2k16_config = os.getenv('P2K16_CONFIG')
-if p2k16_config is not None:
-    app.logger.info("Loading config from {}".format(p2k16_config))
-    app.config.from_envvar('P2K16_CONFIG')
-    app.logger.info("current dir = {}".format(os.path.abspath(os.curdir)))
+class Configuration(metaclass=MetaFlaskEnv):
+    pass
 
 
 class P2k16UserException(Exception):
@@ -27,3 +20,19 @@ class P2k16TechnicalException(Exception):
 
     def __init__(self, msg=None):
         self.msg = msg
+
+
+app = Flask(__name__, static_folder="../web/static")
+
+app.config.BOWER_KEEP_DEPRECATED = False
+app.config['BOWER_COMPONENTS_ROOT'] = '../web/bower_components'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+p2k16_config = os.getenv('P2K16_CONFIG')
+if p2k16_config is not None:
+    app.logger.info("Loading config from {}".format(p2k16_config))
+    app.config.from_envvar('P2K16_CONFIG')
+    app.logger.info("current dir = {}".format(os.path.abspath(os.curdir)))
+
+# Allow the environment variables to override by loading them lastly
+app.config.from_object(Configuration)

2b15f533cd77ee7d3a0b7db3d594d6027e25b168
Trygve Laugstøl <trygvis@inamo.no>
2018-04-17 09:57:36 +0200

    Improving logging some more: o Handling file logging through python so we don't have to rely on   systemd.

--- infrastructure/gunicorn-config.py
+++ infrastructure/gunicorn-config.py
@@ -7,4 +7,9 @@ with open(os.getenv("P2K16_LOGGING")) as f:
 
 logging.config.dictConfig(cfg)
 
+if not os.path.isdir("log"):
+    os.mkdir("log")
+
+accesslog = "log/access.log"
+
 bind = "127.0.0.1:5000"

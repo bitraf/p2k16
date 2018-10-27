80f10aa64c8263d1b389c601d9ced9bf8ce792b3
Håvard Espeland <haavares@forzasys.com>
2018-10-27 16:19:44 +0200

    Remove bogus data.

--- migrations/V001.029__tool.sql
+++ migrations/V001.029__tool.sql
@@ -75,7 +75,3 @@ CREATE TABLE tool_checkout_version
 );
 
 GRANT ALL ON tool_checkout_version TO "p2k16-web";
-
--- Add Bogus data
-INSERT INTO tool_description VALUES (default, NOW(), 1, NOW(), 1, 'cnc', 'Shopbot', 3);
-INSERT INTO tool_description VALUES (default, NOW(), 1, NOW(), 1, 'laser-red', 'Laser Red', 5);

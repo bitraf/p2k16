99dee67c38dfe8723360264039dd3c5af6509bb1
Jon Nordby <jononor@gmail.com>
2019-01-26 21:31:00 +0100

    Send welcome mail to new members
    With CC to post@bitraf.no

--- web/src/p2k16/core/mail/__init__.py
+++ web/src/p2k16/core/mail/__init__.py
@@ -9,6 +9,7 @@ from p2k16.core.models import Account
 logger = logging.getLogger(__name__)
 
 mail_from = ('Bitraf', 'root@bitraf.no')
+signup_cc = 'post@bitraf.no'
 
 
 class Templates():
@@ -16,11 +17,17 @@ class Templates():
         self.basedir = os.path.dirname(__file__)
         env = Environment()
         loader = PackageLoader(__name__, package_path="")
+
         self.send_password_recovery_t = loader.load(env, "send_password_recovery.html")  # type: Template
+        self.new_member_t = loader.load(env, "new_member.html")  # type: Template
 
     def send_password_recovery(self, **kwargs):
         html = self.send_password_recovery_t.render(**kwargs)
         return emails.html(subject="Reset password for Bitraf", html=html)
+
+    def new_member(self, **kwargs):
+        html = self.new_member_t.render(**kwargs)
+        return emails.html(subject="Welcome to Bitraf", html=html)
 
 
 _templates = Templates()
@@ -41,9 +48,20 @@ def send_password_recovery(account: Account, reset_url: str):
     #                              "<p>If you don't want to change your password you can ignore this email.</p>"),
     #                       mail_from=mail_from)
 
-    logger.info("Sending email to {}".format(account.email))
+    logger.info("Sending password recovery email to {}".format(account.email))
 
     m = _templates.send_password_recovery(url=reset_url, account=account)
     m.mail_to = (account.username, account.email)
     m.mail_from = mail_from
     m.send()
+
+def send_new_member(account: Account):
+    logger.info("Sending new member email to {}".format(account.email))
+
+    m = _templates.new_member(account=account)
+    m.mail_to = (account.username, account.email)
+    m.mail_from = mail_from
+    m.cc = signup_cc
+    m.send()
+
+

3d2035b62e46039d5a7d5e3adefa24a8a3dbc870
Andreas Marienborg <omega@palle.net>
2021-08-30 21:45:26 +0200

    force hard redirect on login
    the profile wasnt set properly on first request on login

--- web/src/p2k16/web/static/p2k16/p2k16.js
+++ web/src/p2k16/web/static/p2k16/p2k16.js
@@ -1098,7 +1098,7 @@
      * @param {AuthzService} AuthzService
      * @constructor
      */
-    function UnauthenticatedController($location, $uibModal, P2k16, CoreDataService, AuthzService) {
+    function UnauthenticatedController($location, $window, $uibModal, P2k16, CoreDataService, AuthzService) {
         var self = this;
         self.signupForm = {};
         self.loginForm = {
@@ -1115,7 +1115,7 @@
 
         self.logIn = function () {
             AuthzService.logIn(self.loginForm).then(function () {
-                $location.url("/");
+                $window.location.href = "/";
             });
         };
 
